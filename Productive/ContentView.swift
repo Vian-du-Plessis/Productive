d39d2c00ee218f803aae8ed5bdf322eb98766d11
Vian du Plessis <90850796+Vian-du-Plessis@users.noreply.github.com>
2022-09-28 14:22:20 +0200

    Initial Commit

---
+++ Productive/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Productive
+//
+//  Created by Elzine Brummelkamp on 2022/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Productive/ProductiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductiveApp.swift
+//  Productive
+//
+//  Created by Elzine Brummelkamp on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct ProductiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
